68269921ddbcef6690b598fe2a5d0d049f794c31
ana <anameciarobalo@gmail.com>
2020-01-01 20:56:33 +0000

    event index style added

--- app/controllers/hosts_sessions_controller.rb
+++ app/controllers/hosts_sessions_controller.rb
@@ -6,10 +6,18 @@ class HostsSessionsController < ApplicationController
 
     def create
         host = Host.find_by(hostname:params[:host][:hostname])
-        host = host.try(:authenticate, params[:host][:password])
-        return redirect_to hostlogin_path unless host
-        session[:host_id] = host.id
-        redirect_to host_path(host.id)
+        # host = host.try(:authenticate, params[:host][:password])
+        # return redirect_to hostlogin_path unless host
+        # session[:host_id] = host.id
+        # redirect_to host_path(host.id)
+        if host.try(:authenticate, params[:host][:password])
+            session[:host_id] = host.id
+            redirect_to host_path(host.id)
+        else
+            flash[:notice] = "Sorry, we can't find a host with that hostname and password"
+            redirect_to hostlogin_path
+        end
+
     end 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :bookings
     has_many :events, through: :bookings
 
-    has_secure_password
+    has_secure_password    
 
     def self.inactive_users
         result =   User.all.filter{|user|user.bookings == []}
