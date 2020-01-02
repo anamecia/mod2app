c80163a53c8fafcfbbbeb693a295e1b966c4614b
ana <anameciarobalo@gmail.com>
2020-01-02 09:20:58 +0000

    style added

--- app/controllers/admins_sessions_controller.rb
+++ app/controllers/admins_sessions_controller.rb
@@ -5,10 +5,17 @@ class AdminsSessionsController < ApplicationController
 
     def create
         admin = Admin.find_by(adminname:params[:admin][:adminname])
-        admin = admin.try(:authenticate, params[:admin][:password])
-        return redirect_to adminlogin_path unless admin
-        session[:admin_id] = admin.id
-        redirect_to adminhomepage_path
+        # admin = admin.try(:authenticate, params[:admin][:password])
+        # return redirect_to adminlogin_path unless admin
+        # session[:admin_id] = admin.id
+        # redirect_to adminhomepage_path
+        if admin.try(:authenticate, params[:admin][:password])
+            session[:admin_id] = admin.id
+            redirect_to admin_path(admin.id)
+        else
+            flash[:notice] = "Sorry, we can't find an admin with that adminname and password"
+            redirect_to adminlogin_path
+        end
     end 
 
     def destroy
--- app/controllers/users_sessions_controller.rb
+++ app/controllers/users_sessions_controller.rb
@@ -6,10 +6,17 @@ class UsersSessionsController < ApplicationController
 
     def create
         user = User.find_by(username:params[:user][:username])
-        user = user.try(:authenticate, params[:user][:password])
-        return redirect_to userlogin_path unless user
-        session[:user_id] = user.id
-        redirect_to user_path(user.id)
+        # user = user.try(:authenticate, params[:user][:password])
+        # return redirect_to userlogin_path unless user
+        # session[:user_id] = user.id
+        # redirect_to user_path(user.id)
+        if user.try(:authenticate, params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user.id)
+        else
+            flash[:notice] = "Sorry, we can't find a user with that username and password"
+            redirect_to userlogin_path
+        end
     end 
 
     def destroy
